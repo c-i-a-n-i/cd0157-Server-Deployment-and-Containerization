344272ddc158f8b6fc2d533256a9c7eb69e5f41d
ifani <ifeatu96@gmail.com>
2022-01-10 00:46:51 -0800

    udpating failed test

--- test_main.py
+++ test_main.py
@@ -25,7 +25,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == 'Healthy'
+    assert response.json == 'Healthy v0.0.2'
 
 
 def test_auth(client):
